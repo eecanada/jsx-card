9fe18ba3464af2179ea9ba8a48f1b32373d70cc4
Eder Canada <canada.eder@gmail.com>
2021-10-07 12:16:57 -0700

    jsx - html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const buttonWords = {
+  text:'click me!!'
+}
+
 const App = () => {
-  return <div>Hello there!</div>;
+  const buttonText = 'click me!';
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ color: 'white', backgroundColor: 'blue' }}>
+        {buttonWords.text}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
