4e1bb8a6fd2faee39b62a21e37647714f2abd1a6
Eder Canada <canada.eder@gmail.com>
2021-10-07 15:22:29 -0700

    add faker

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const buttonWords = {
-  text:'click me!!'
-}
+import faker from 'faker';
 
 const App = () => {
-  const buttonText = 'click me!';
   return (
-    <div>
-      <label className="label" htmlFor="name">
-        Enter Name:
-      </label>
-      <input id="name" type="text" />
-      <button style={{ color: 'white', backgroundColor: 'blue' }}>
-        {buttonWords.text}
-      </button>
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
     </div>
   );
 };
-
 ReactDOM.render(<App />, document.querySelector('#root'));
